7d627d8722cca1c4cf64bff8bb89653054e3d605
rozhkovra <rozhkovra@gmail.com>
2017-08-31 15:48:36 +0600

    EKIN-0068 Add payments table

--- easykin/src/main/java/ru/rrozhkov/easykin/db/impl/DumpManager.java
+++ easykin/src/main/java/ru/rrozhkov/easykin/db/impl/DumpManager.java
@@ -3,11 +3,13 @@ package ru.rrozhkov.easykin.db.impl;
 import ru.rrozhkov.easykin.FilesSettings;
 import ru.rrozhkov.easykin.context.MasterDataContext;
 import ru.rrozhkov.easykin.model.category.ICategory;
+import ru.rrozhkov.easykin.model.fin.payment.IPayment;
 import ru.rrozhkov.easykin.model.person.IPerson;
 import ru.rrozhkov.easykin.model.task.ITask;
 import ru.rrozhkov.easykin.model.task.Status;
 import ru.rrozhkov.easykin.module.Module;
 import ru.rrozhkov.easykin.module.ModuleManager;
+import ru.rrozhkov.easykin.payment.impl.convert.PaymentInsertConverter;
 import ru.rrozhkov.easykin.person.auth.AuthManager;
 import ru.rrozhkov.easykin.person.impl.convert.PersonInsertConverter;
 import ru.rrozhkov.easykin.task.impl.convert.CategoryInsertConverter;
@@ -35,6 +37,9 @@ public class DumpManager {
         TaskInsertConverter tConverter = new TaskInsertConverter();
         for (ITask task : FilterUtil.filter((Collection<ITask>) ModuleManager.invoke(Module.TASK, "tasks", AuthManager.instance().signedPerson()), TaskFilterFactory.status(Status.OPEN)))
             builder.append(tConverter.convert(task)+";");
+        PaymentInsertConverter payConverter = new PaymentInsertConverter();
+        for (IPayment payment : (Collection<IPayment>) ModuleManager.invoke(Module.FIN, "finance"))
+            builder.append(payConverter.convert(payment)+";");
         writeDump(builder.toString());
     }
     public static void writeDump(String s){
