76ba4b39b16714eeed45bd0acb5f4f8aad824003
rozhkovra <rozhkovra@gmail.com>
2018-04-03 15:31:25 +0600

    EKIN-0108 Add parse house payment

--- easykin.service/src/main/java/ru/rrozhkov/easykin/service/gui/style/impl/custom/ServiceCalcConverter.java
+++ easykin.service/src/main/java/ru/rrozhkov/easykin/service/gui/style/impl/custom/ServiceCalcConverter.java
@@ -24,8 +24,7 @@ public class ServiceCalcConverter extends CollectionConverter<ServiceCalc> {
 				, getCalculator(ServiceCalcUtil.getCalcByType(entry,CalculationType.HEATING)).calculate().toString()
 				, getCalculator(ServiceCalcUtil.getCalcByType(entry,CalculationType.ANTENNA)).calculate().toString()
 				, getCalculator(ServiceCalcUtil.getCalcByType(entry,CalculationType.INTERCOM)).calculate().toString()
-//				, getCalculator(ServiceCalcUtil.getCalcByType(entry,CalculationType.HOUSE)).calculate().toString()
-				, "0"
+				, getCalculator(ServiceCalcUtil.getCalcByType(entry,CalculationType.HOUSE)).calculate().toString()
 				, getCalculator(ServiceCalcUtil.getCalcByType(entry,CalculationType.REPAIR)).calculate().toString()
 				, getCalculator(entry).calculate().toString()+(!ServiceCalcUtil.getNoPaidSum(entry).free()?" ("+ServiceCalcUtil.getNoPaidSum(entry)+")":"")};
 	}
