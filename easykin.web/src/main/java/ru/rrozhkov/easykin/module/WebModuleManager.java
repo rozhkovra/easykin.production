efb1443afd9215ee7ac16ba6af9151b012c0069e
Roman Rozhkov <rrozhkov@luxoft.com>
2018-07-05 14:43:28 +0600

    EKIN-0233 Null on dashboard

--- easykin.web/src/main/java/ru/rrozhkov/easykin/module/WebModuleManager.java
+++ easykin.web/src/main/java/ru/rrozhkov/easykin/module/WebModuleManager.java
@@ -24,7 +24,7 @@ public class WebModuleManager {
     }
 
     public String moduleName() {
-        return Module.name(module());
+        return module().isEmpty()?"":Module.name(module());
     }
 
     public String url() {
