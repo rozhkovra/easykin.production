93bd7e422d26aa56f06d12c6d6f03e106e5d4f3e
Roman Rozhkov <rrozhkov@luxoft.com>
2018-11-23 17:11:12 +0600

    EKIN-0292 Edit payment

--- easykin.fin/src/main/java/ru/rrozhkov/easykin/payment/gui/PaymentForm.java
+++ easykin.fin/src/main/java/ru/rrozhkov/easykin/payment/gui/PaymentForm.java
@@ -1,25 +1,41 @@
 package ru.rrozhkov.easykin.payment.gui;
 
+import ru.rrozhkov.easykin.core.collection.CollectionUtil;
 import ru.rrozhkov.easykin.core.gui.Form;
 import ru.rrozhkov.easykin.core.gui.IGUIEditor;
+import ru.rrozhkov.easykin.model.fin.Money;
 import ru.rrozhkov.easykin.model.fin.payment.IPayment;
 import ru.rrozhkov.easykin.core.util.DateUtil;
+import ru.rrozhkov.easykin.model.fin.payment.PaymentCategory;
+import ru.rrozhkov.easykin.model.fin.payment.PaymentStatus;
+import ru.rrozhkov.easykin.model.fin.payment.impl.PaymentFactory;
+import ru.rrozhkov.easykin.model.task.ITask;
+import ru.rrozhkov.easykin.payment.db.impl.PaymentHandler;
 
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
 import java.awt.Component;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Date;
 
 
 public class PaymentForm extends Form {
 	private static final long serialVersionUID = 1L;
-	private Component commentField;
-	private Component amountField;
-	private Component dateField;
+	private static final PaymentFactory paymentFactory = PaymentFactory.instance();
+	private static final PaymentHandler paymentHandler = PaymentHandler.instance();
+	private JTextField commentField;
+	private JTextField amountField;
+	private JTextField dateField;
 	private Component nameLabel;
 	private Component priceLabel;
 	private Component dateLabel;
 	private IPayment payment;
-	private Component categoryField;
+	private JComboBox categoryComboBox;
 	private Component categoryLabel;
-	
+	private JComboBox statusComboBox;
+	private Component statusLabel;
+
 	public static Form create(final IGUIEditor parent, final IPayment payment) {
 		Form form = new PaymentForm(parent, payment);
 		form.fill();
@@ -29,11 +45,10 @@ public class PaymentForm extends Form {
 	private PaymentForm(IGUIEditor parent, IPayment payment) {
 		super(parent);
 		this.payment = payment;
-		fill();
 	}
 	
 	public void fill(){
-		setLayout(guiFactory.gridLayout(6,2));
+		setLayout(guiFactory.gridLayout(7,2));
 		add(guiFactory.labelEmpty());
 		add(guiFactory.labelEmpty());
 		add(getCommentLabel()); 
@@ -42,38 +57,57 @@ public class PaymentForm extends Form {
 		add(getAmountField()); 
 		add(getDateLabel()); 
 		add(getDateField());
-		add(getCategoryLabel()); 
-		add(getCategoryField());
-		add(guiFactory.labelEmpty());
+		add(getCategoryLabel());
+		add(getCategoryComboBox());
+		add(getStatusLabel());
+		add(getStatusComboBox());
+		add(getOkButton());
 		add(getCancelButton());
 	}
 
-	private Component getCommentField(){
+	private JTextField getCommentField(){
 		if(commentField == null){
-			commentField = guiFactory.fieldReadOnly(50, payment.getComment());
+			commentField = (JTextField)guiFactory.fieldEditable(50, payment.getComment());
 		}
 		return commentField;
 	}
 
-	private Component getAmountField(){
+	private JTextField getAmountField(){
 		if(amountField == null){
-			amountField = guiFactory.fieldReadOnly(10, payment.getAmount().toString());
+			amountField = (JTextField)guiFactory.fieldEditable(10, payment.getAmount().toString());
 		}
 		return amountField;
 	}
 	
-	private Component getDateField(){
+	private JTextField getDateField(){
 		if(dateField == null){
-			dateField = guiFactory.fieldReadOnly(10, DateUtil.format(payment.getDate()));
+			dateField = (JTextField)guiFactory.fieldEditable(10, DateUtil.format(payment.getDate()));
 		}
 		return dateField;
 	}
 	
-	private Component getCategoryField(){
-		if(categoryField == null){
-			categoryField = guiFactory.fieldReadOnly(50, payment.getCategory().toString());
+	private JComboBox getCategoryComboBox(){
+		if(categoryComboBox == null){
+			categoryComboBox = (JComboBox)guiFactory.comboBoxFilled(CollectionUtil.create(
+					PaymentCategory.AUTO,
+					PaymentCategory.SERVICE,
+					PaymentCategory.PAYMENT,
+					PaymentCategory.TASK
+			));
+			categoryComboBox.setSelectedItem(payment.getCategory());
 		}
-		return categoryField;
+		return categoryComboBox;
+	}
+
+	private JComboBox getStatusComboBox(){
+		if(statusComboBox == null){
+			statusComboBox = (JComboBox)guiFactory.comboBoxFilled(CollectionUtil.create(
+					PaymentStatus.PLAN,
+					PaymentStatus.FACT
+			));
+			statusComboBox.setSelectedItem(payment.getStatus());
+		}
+		return statusComboBox;
 	}
 	
 	private Component getCommentLabel(){
@@ -98,5 +132,41 @@ public class PaymentForm extends Form {
 		if(categoryLabel == null)
 			categoryLabel = guiFactory.label("Категория");
 		return categoryLabel;
+	}
+
+	private Component getStatusLabel(){
+		if(statusLabel == null)
+			statusLabel = guiFactory.label("Статус");
+		return statusLabel;
+	}
+
+	protected void update() {
+		payment = paymentFactory.createPayment(payment.getId(), (PaymentCategory)getCategoryComboBox().getSelectedItem(),
+				getCommentField().getText(), Money.valueOf(getAmountField().getText().replace(" ","")),
+				DateUtil.parse(getDateField().getText()), (PaymentStatus)getStatusComboBox().getSelectedItem());
+	}
+
+	protected void ok() {
+		update();
+		if (!validateData()) {
+			return;
+		}
+		try {
+			paymentHandler.update(payment);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+//		if (getRepeatCheckBox().isSelected()) {
+//			if (!validatePeriodic()) {
+//				return;
+//			}
+//			Period period = (Period)periodComboBox.getSelectedItem();
+//			Date untilDate = DateUtil.parse(untilDateField.getText());
+//			Collection<ITask> tasks = periodTaskBuilder.build(period, untilDate, task);
+//			taskService.createOrUpdate(tasks);
+//		} else {
+//			taskService.createOrUpdate(task);
+//		}
+		parent.refresh();
 	}
 }
