e05a0da4aedb669694966a4ca6d21e18ef711b7b
Roman Rozhkov <rrozhkov@luxoft.com>
2019-02-18 17:11:58 +0600

    EKIN-0328 Update service statistics

--- easykin.web/src/main/webapp/js/easykin.service.js
+++ easykin.web/src/main/webapp/js/easykin.service.js
@@ -1,11 +1,15 @@
 $( document ).ready(
 $(function () {
+    $('#addRateBtn').click(function () {
+        $('#addRateDiv').show()
+    })
+
     $('#services').DataTable()
 
     var barChartData = {
       labels  : ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
       datasets: [
-        {
+/*        {
           label               : '2017',
           fillColor           : '#00a65a',
           strokeColor         : '#00a65a',
@@ -15,16 +19,28 @@ $(function () {
           pointHighlightStroke: 'rgba(60,141,188,1)',
           data                : [5146.78, 5031.61, 5291.60, 4401.07, 3762.17, 4185.63, 4113.96, 4223.81, 4412.46, 4523.19, 4686.04, 4621.23]
         },
+*/
         {
           label               : '2018',
-          fillColor           : 'rgba(60,141,188,0.9)',
-          strokeColor         : 'rgba(60,141,188,0.8)',
-          pointColor          : '#3b8bba',
+          fillColor           : '#00a65a',
+          strokeColor         : '#00a65a',
+          pointColor          : '#00a65a',
           pointStrokeColor    : 'rgba(60,141,188,1)',
           pointHighlightFill  : '#fff',
           pointHighlightStroke: 'rgba(60,141,188,1)',
           data                : [4760.06, 4779.48, 4900.75, 4967.82, 4707.71, 4967.82, 4482.05, 5019.84, 4754.35, 4752.53, 4827.94, 5026.65]
-        }
+        },
+         {
+           label               : '2019',
+           fillColor           : 'rgba(60,141,188,0.9)',
+           strokeColor         : 'rgba(60,141,188,0.8)',
+           pointColor          : '#3b8bba',
+           pointStrokeColor    : 'rgba(60,141,188,1)',
+           pointHighlightFill  : '#fff',
+           pointHighlightStroke: 'rgba(60,141,188,1)',
+           data                : [4234.87, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+         }
+
       ]
     }
 
